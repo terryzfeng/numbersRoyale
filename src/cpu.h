aed7a3f3ed2d99c821ad4850974aae527ac56267
terry feng <fenglyfe@gmail.com>
2024-12-08 22:20:46 -0800

    CPU class (#5)
    * CPU class
    
    * add is_cpu function to CPU player class

---
+++ src/cpu.h
@@ -0,0 +1,43 @@
+/**
+ * @file cpu.h
+ * @brief CPU logic for Numbers Royale player
+ * The CPU player selects a random number from the available set. 
+ */
+#include "player.h"
+
+class CPU : public Player {
+public:
+  /**
+   * @brief Initialize the CPU player logic
+   */
+  CPU();
+
+  /**
+   * @brief Get the CPU player name
+   * 
+   * @return std::string CPU name
+   */
+  std::string name() const override {
+    return "CPU";
+  }
+
+  /**
+   * @brief Return if the player is a CPU
+   * 
+   * @return true for CPU player
+   */
+  bool is_cpu() const override {
+    return true;
+  }
+
+  /**
+   * @brief Select a move for the CPU player
+   * 
+   * Override the move function from the Player class.
+   * Will ignore the move number and select a random number
+   * from the moves set.
+   * 
+   * @param _number (ignored) the number to move
+   */
+  unsigned int move(unsigned int _number) override;
+};
